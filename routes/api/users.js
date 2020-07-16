a7aff57c7d746225f96cf690dc93276efe66711b
jungsikjeong <wndtlr1024@gmail.com>
2020-07-16 16:29:30 +0900

    스키마모델설정

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @route   Get api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', '이름을 입력하세요').not().isEmpty(),
+    check('email', '유효한 이메일을 포함해주세요').isEmail(),
+    check('password', '6 자 이상의 비밀번호를 입력해주세요').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    // validationResult: 유효성 검사 결과
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
